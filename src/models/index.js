623945e426730bd6c4b4f55eb2b165dfc6392f94
Mario Lanes <mario.lanesjr@gmail.com>
2025-05-08 19:04:36 +0100

    refactor(models): restructure models into entities
    - Move models to src/models/entities
    - Update model loader to use new structure
    - Fix database config import path

---
+++ src/models/index.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const { Sequelize } = require('sequelize');
+const config = require('../config/database');
+const basename = path.basename(__filename);
+
+const environment = process.env.NODE_ENV || 'development';
+const dbConfig = config[environment];
+
+const sequelize = new Sequelize(dbConfig.db, {
+  dialect: dbConfig.dialect,
+  logging: dbConfig.logging,
+  ssl: dbConfig.dialectOptions.ssl,
+  define: { underscored: true },
+  pool: {
+    max: 32,
+    idle: 30000,
+    acquire: 300000,
+  },
+});
+
+const db = {};
+
+// 1. Read every .js file in models/entities/ except this index.js
+const entitiesPath = path.join(__dirname, 'entities');
+fs
+  .readdirSync(entitiesPath)
+  .filter(file => (
+    file.indexOf('.') !== 0 &&
+    file !== basename &&
+    file.slice(-3) === '.js'
+  ))
+  .forEach(file => {
+    const defineModel = require(path.join(entitiesPath, file));
+    const model = defineModel(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+// 2. Run all `associate()` methods to hook up relationships
+Object.values(db)
+  .filter(model => typeof model.associate === 'function')
+  .forEach(model => model.associate(db));
+
+// 3. Export
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
